ca140bf8f00ac25fcd547eb371f4e2bf0f1b75de
Rafael Abonce <mrraxyer@hotmail.com>
2026-05-07 01:09:06 -0600

    repo: local files on track

---
+++ src/main/java/dev/mrraxyer/sportstournamentmanager/SportsTournamentManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.mrraxyer.sportstournamentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsTournamentManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsTournamentManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mrraxyer/sportstournamentmanager/SportsTournamentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mrraxyer.sportstournamentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsTournamentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
